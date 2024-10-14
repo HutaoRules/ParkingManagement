368abc1fe12ba87113c665d882ba41504b9f08ac
TrungNV <trung.work203@gmail.com>
2024-10-14 09:21:54 +0700

    fix run bug

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,11 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    
     db.init_app(app)
     
-    from app import routes
-    app.register_blueprint(routes.bp)
-
+    with app.app_context():
+        from app.routes import routes
+        app.register_blueprint(routes)
+    
     return app
